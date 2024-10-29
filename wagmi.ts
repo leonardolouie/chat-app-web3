b33ff71628d1b17dbbcea9bb58b0f39258404a4f
leonardo louie ordonez <leonardolouie30@gmail.com>
2024-10-30 01:53:08 +0800

    feat: init commit

---
+++ wagmi.ts
@@ -0,0 +1,38 @@
+import { getDefaultConfig, Chain } from "@rainbow-me/rainbowkit";
+import {
+  arbitrum,
+  base,
+  mainnet,
+  optimism,
+  polygon,
+  sepolia,
+} from "wagmi/chains";
+
+const localChain: Chain = {
+  id: 31337, //hardhat node chain
+  name: "localhost:8545",
+  nativeCurrency: {
+    name: "Local ETH",
+    symbol: "ETH",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: { http: ["http://localhost:8545"] },
+  },
+  testnet: true,
+};
+
+export const config = getDefaultConfig({
+  appName: "RainbowKit demo",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [
+    localChain,
+    mainnet,
+    polygon,
+    optimism,
+    arbitrum,
+    base,
+    ...(process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true" ? [sepolia] : []),
+  ],
+  ssr: true,
+});
